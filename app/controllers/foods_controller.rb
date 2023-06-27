128474df84d3e8b3c52818407a5f184b0721876d
isaacg <iwanyoike65@gmail.com>
2023-06-27 12:16:49 +0300

    Fix controller errors

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,4 @@
 class FoodsController < ApplicationController
-  before_action :set_food, only: %i[ show edit update destroy ]
-
   # GET /foods or /foods.json
   def index
     @foods = Food.all
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,4 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: %i[ show edit update destroy ]
-
   # GET /recipes or /recipes.json
   def index
     @recipes = Recipe.all
