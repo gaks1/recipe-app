026a545f5adc46f483d913a50b363cf666a3f433
raihan2bd <raihan2bd.official@gmail.com>
2023-06-27 17:54:30 +0600

    Fix linter errors from this project

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  
+
   validates :name, presence: true
   validates :preparation_time, numericality: { only_integer: true, greater_or_equal_to: 0 }
   validates :cooking_time, numericality: { only_integer: true, greater_or_equal_to: 0 }
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,17 +1,17 @@
-require "test_helper"
+require 'test_helper'
 
 class RecipesControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  test 'should get new' do
     get recipes_new_url
     assert_response :success
   end
 
-  test "should get create" do
+  test 'should get create' do
     get recipes_create_url
     assert_response :success
   end
 
-  test "should get index" do
+  test 'should get index' do
     get recipes_index_url
     assert_response :success
   end
