f1038f1cc8f06b0d126326cc4074dc4ffaa8f0b3
raihan2bd <raihan2bd.official@gmail.com>
2023-06-28 17:28:22 +0600

    Add a route for public recipes

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
     resources :recipe_foods, only: [:new, :create, :destroy, :edit, :update]
   end
   match 'recipes/:recipe_id' => 'recipes#toggle_public', as: :toggle_public, via: :patch
+  get "public_recipes", to: "public_recipes#index"
 end
