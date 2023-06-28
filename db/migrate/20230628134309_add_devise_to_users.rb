426137b201d6cf9d5d6e2ee6b0e9d9178b320c36
isaacg <iwanyoike65@gmail.com>
2023-06-28 17:51:35 +0300

    Add unconfirm column to users table

--- db/migrate/20230628134309_add_devise_to_users.rb
+++ db/migrate/20230628134309_add_devise_to_users.rb
@@ -25,7 +25,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
       t.string   :confirmation_token
       t.datetime :confirmed_at
       t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
@@ -44,8 +44,15 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
   end
 
   def self.down
-    # By default, we don't want to make any assumption about how to roll back a migration when your
-    # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
+    remove_index :users, column: :email
+    remove_index :users, column: :reset_password_token
+    remove_index :users, column: :confirmation_token
+  
+    change_table :users do |t|
+      t.remove :email, :encrypted_password
+      t.remove :reset_password_token, :reset_password_sent_at
+      t.remove :remember_created_at
+      t.remove :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email
+    end
   end
 end
