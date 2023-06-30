2188a7902b6a8a897701981a7acb43f08b147a00
raihan2bd <raihan2bd.official@gmail.com>
2023-06-30 14:43:34 +0600

    Fix edit method from recipe foods controller

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -14,9 +14,6 @@ class RecipeFoodsController < ApplicationController
     @recipe_food = RecipeFood.new
   end
 
-  # GET /recipe_foods/1/edit
-  def edit; end
-
   # POST /recipe_foods or /recipe_foods.json
   def create
     @recipe_food = RecipeFood.new(recipe_food_params)
